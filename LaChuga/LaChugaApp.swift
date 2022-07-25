b6c1a8cd84224592bd7ae3fea6966232c5a51215
Anton Makarov <makarov070786@mail.ru>
2022-07-25 22:21:37 +0300

    Initial Commit

---
+++ LaChuga/LaChugaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaChugaApp.swift
+//  LaChuga
+//
+//  Created by Антон Макаров on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LaChugaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
